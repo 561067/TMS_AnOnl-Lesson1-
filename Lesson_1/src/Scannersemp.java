80b9a71606593b6518608455a7fd424f24a467ac
561067 <153899946+561067@users.noreply.github.com>
2023-12-17 19:07:37 +0200

    Add files via upload

---
+++ Lesson_1/src/Scannersemp.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Scannersemp{
+    public static void main(String[] args) {
+        Scanner k = new Scanner(System.in);
+        System.out.println("ВВЕДИТЕ 2 ЧИСЛА ");
+        int number1 = k.nextInt(); //считывать число №1
+        int number2 = k.nextInt(); //считывать число №24
+
+        System.out.println(number1 + number2);
+}
+
+}
